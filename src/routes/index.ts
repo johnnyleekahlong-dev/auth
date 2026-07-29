496acfb9f01cafb055d1dde88bcb121c586c17cc
Lee Kahlong <KahlongLJ_RicohOSSD@binnies.com>
2026-07-29 12:21:24 +0800

    verification url

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -15,7 +15,7 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
 router.post('/logout', logout);
-router.post('/verify-account/:verificationCode', verifyAccount);
+router.get('/verify-account/:verificationCode', verifyAccount);
 router.get('/get-me', isAuth, getMe);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password/:resetToken', resetPassword);
